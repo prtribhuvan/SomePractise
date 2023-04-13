08e8f9c48323693ccc7d98b62cab754fbdc7e335
prtribhuvan <pallavitribhuvan1997@gmail.com>
2023-04-13 22:03:22 +0530

    Addition of triangles

---
+++ Practice1/src/com/practice/Triangle.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		
+	
+	
+	int a =10;
+	int b =20;
+	int c = 30;
+	
+	System.out.println("Addition of triangle "+(a+b+c));
+	
+	
+	
+	
+	
+	}	
+	
+	
+}
